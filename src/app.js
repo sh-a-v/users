4b4a6576f90e56c1ddb4ef7f44e9dbc62ec087d4
Alexander Shershnev <shershnev.av@gmail.com>
2016-09-22 06:17:49 +0300

    fix test

--- src/app.js
+++ src/app.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
+import store from './store';
 import Header from './shared/components/header';
 import Users from './containers/users';
 import './app.css';
 
+// Remove this when material-ui@0.16.0 will be released
+injectTapEventPlugin();
+
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Header />
-        <div className="app-content">
-          <Users />
-        </div>
-      </div>
+      <Provider store={store}>
+        <MuiThemeProvider>
+          <div className="app">
+            <Header />
+            <div className="app-content">
+              <Users />
+            </div>
+          </div>
+        </MuiThemeProvider>
+      </Provider>
     );
   }
 }
